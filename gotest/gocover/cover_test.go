4a96f69fb7206ba7db792b3fb473ce4d09f51012
Basant <basant.kumar93@gmail.com>
2021-04-03 13:49:45 +0530

    add benchmark test

--- gotest/gocover/cover_test.go
+++ gotest/gocover/cover_test.go
@@ -17,3 +17,17 @@ func TestAdder(t *testing.T) {
 //analyse the number of times the statement is covered
 //go test -covermode count -coverprofile cover.out github.com/basant93/goexamples/gotest/gocover/
 //scale of the coverage
+
+func BenchmarkAdder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		adder(5, 7)
+	}
+}
+
+//Run Benchmark test
+//go test -bench Adder github.com/basant93/goexamples/gotest/gocover/
+//-bench flag is required. Adder is a regexp to match benchmark test
+// . is used to run all test
+//go test -benchtime 5s -bench . github.com/basant93/goexamples/gotest/gocover/
+//to get idea of memory being used
+//go test -benchtime 5s -bench . -benchmem github.com/basant93/goexamples/gotest/gocover/
